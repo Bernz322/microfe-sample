6323199b01c4417cb8925b969a16dbff1396e0c8
Jeffrey Bernadas <bernadas_jeffrey@yahoo.com>
2023-02-01 16:56:45 +0800

    add partial SearchableSelect component

--- complex-mfe/core/src/App.tsx
+++ complex-mfe/core/src/App.tsx
@@ -9,35 +9,141 @@ import {
   MBODrawer,
   TextAvatar,
   CircularProgress2,
+  SearchableSelect,
 } from "./components";
 import { Box, Container } from "./layout";
 
 import "./index.scss";
 import { CircularProgress } from "@mui/material";
 
-const App = () => (
-  <div className="mt-10 text-3xl mx-auto max-w-6xl">
-    <div>The following components are available: </div>
-    <MBOAppBar />
-    <MBODrawer />
-    <Box />
-    <Container sx={{ m: 10 }}>
-      <Grid container>
-        <Grid item xs={3}>
-          <Button>Hello</Button>
+const options: IOptions[] = [
+  { name: "Option 1", value: "1", id: "1" },
+  { name: "Option 2", value: "2", id: "2" },
+];
+
+interface IOptions {
+  name: string;
+  value: string;
+  id: string;
+}
+
+const App = () => {
+  const [progress, setProgress] = React.useState(0);
+  const [value, setValue] = React.useState<IOptions>(options[0]); // Value of the option selected by the user
+  const [inputValue, setInputValue] = React.useState(""); // Value displayed in the textbox
+
+  // React.useEffect(() => {
+  //   const timer = setInterval(() => {
+  //     setProgress((oldProgress) => (oldProgress >= 100 ? 0 : oldProgress + 10));
+  //   }, 500);
+
+  //   return () => {
+  //     clearInterval(timer);
+  //   };
+  // }, []);
+
+  // console.log('test');
+
+  const onChangeHandler = (event, value: IOptions) => {
+    console.log(value)
+    setValue(value);
+  };
+
+  const onInputChangeHandler = (event, newInputValue: string) => {
+    setInputValue(newInputValue);
+  };
+
+  return (
+    <div className="mt-10 text-3xl mx-auto max-w-6xl">
+      <div>The following components are available: </div>
+      <MBOAppBar />
+      <MBODrawer />
+      <Box />
+      <Container sx={{ m: 10 }}>
+        <Grid container>
+          <Grid item xs={3}>
+            <Button id="3">Hello</Button>
+          </Grid>
+          <Grid item xs={4}>
+            <TextField label="A text field" />
+          </Grid>
+          <Grid item xs={4}>
+            <Typography>Typography</Typography>
+          </Grid>
         </Grid>
-        <Grid item xs={4}>
-          <TextField label="A text field" />
+
+        <h1 className="text-2xl mb-5">SearchableSelect Component Below</h1>
+        <div>
+          <p>{`value: ${value !== null ? `${value}` : "null"}`}</p>
+          <p>{`inputValue: '${inputValue}'`}</p>
+          <br />
+          <SearchableSelect
+            onChange={onChangeHandler}
+            onInputChange={onInputChangeHandler}
+            value={value}
+            inputValue={inputValue}
+            optionData={options}
+          />
+        </div>
+        <h1 className="text-2xl my-5">Text Avatar Component Below</h1>
+        <Grid container>
+          <Grid xs={2} item>
+            <TextAvatar text="JB" isSelected={false} />
+            <Typography>Not selected</Typography>
+          </Grid>
+          <Grid xs={10} item>
+            <TextAvatar text="CB" isSelected />
+            <Typography>Selected</Typography>
+          </Grid>
         </Grid>
-        <Grid item xs={4}>
-          <Typography>Typography</Typography>
+        <h1 className="text-2xl my-5 ">Circular Progress 2 Component Below</h1>
+
+        <Grid container>
+          <Grid xs={2} item>
+            <CircularProgress2
+              size={85}
+              thickness={1.5}
+              style={{
+                color: "#74afc7",
+              }}
+            />
+            <Typography>MBO 2nd Blank Page Circular Progress</Typography>
+          </Grid>
+          <Grid xs={10} item>
+            <CircularProgress2
+              size={85}
+              thickness={5.5}
+              style={{
+                margin: "0 10px",
+              }}
+              value={progress}
+              variant="static"
+            />
+            <Typography>Static variant with value</Typography>
+          </Grid>
+          <Grid xs={10} item>
+            <CircularProgress2
+              size={85}
+              thickness={5.5}
+              style={{
+                margin: "0 10px",
+              }}
+              value={progress}
+              variant="determinate"
+            />
+            <Typography>Determinate variant with value</Typography>
+          </Grid>
+          <Grid xs={10} item>
+            <CircularProgress2 size={85} thickness={1.5} />
+            <Typography>Indeterminate variant</Typography>
+          </Grid>
+          <Grid xs={10} item>
+            <CircularProgress />
+            <Typography>Default MUI Circular Progress</Typography>
+          </Grid>
         </Grid>
-      </Grid>
-      <TextAvatar text="Y" isSelected />
-      <TextAvatar text="N" isSelected={false} />
-      <CircularProgress />
-      <CircularProgress2 size={60}/>
-    </Container>
-  </div>
-);
+      </Container>
+    </div>
+  );
+};
 ReactDOM.render(<App />, document.getElementById("app"));
--- complex-mfe/core/src/components/CoreSearchableSelect/index.tsx
+++ complex-mfe/core/src/components/CoreSearchableSelect/index.tsx
@@ -1,7 +1,89 @@
 import React from "react";
+import TextField from "@mui/material/TextField";
+import Autocomplete, { AutocompleteProps } from "@mui/material/Autocomplete";
+import Box from "@mui/material/Box";
+import { styled } from "@mui/material/styles";
 
-const SearchableSelect = () => {
-  return <div>SearchableSelect</div>;
+interface AutocompleteOptionData {
+  // All expected values that are going to be rendered in the option field
+  name: string;
+  value: string;
+  id: string;
+}
+
+interface Props<T extends AutocompleteOptionData> {
+  autocompleteProps?: AutocompleteProps<
+    T,
+    boolean | undefined,
+    boolean | undefined,
+    boolean | undefined
+  >;
+  isClearable?: boolean;
+  isDisabled?: boolean;
+  value: T;
+  onChange: (
+    event: React.ChangeEvent<{}>,
+    value: T,
+    reason?: "selectOption"
+  ) => void;
+  inputValue: string;
+  onInputChange: (event: React.ChangeEvent<{}>, value: string) => void;
+  optionData: T[];
+  error?: boolean;
+}
+
+const SearchableSelect = <T extends AutocompleteOptionData>(
+  props: Props<T>
+) => {
+  const {
+    autocompleteProps,
+    isClearable,
+    isDisabled,
+    value,
+    onChange,
+    inputValue,
+    onInputChange,
+    optionData,
+    error = value === null,
+  } = props;
+
+  return (
+    <StyledContainer>
+      <Autocomplete
+        {...autocompleteProps}
+        value={value}
+        onChange={(event, value) => onChange(event, value as T)}
+        disabled={isDisabled}
+        disableClearable={isClearable}
+        inputValue={inputValue}
+        onInputChange={onInputChange}
+        sx={{ width: 300 }}
+        options={optionData}
+        getOptionLabel={(option) => option["name"]}
+        isOptionEqualToValue={(option, value) => option.value === value.value}
+        noOptionsText="No options"
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            error={error}
+            label="Select option"
+            helperText={error && "Error msg goes here"}
+          />
+        )}
+        renderOption={(props, options) => {
+          return (
+            <Box component="li" {...props} key={options.id}>
+              {options.name} - {options.value}
+            </Box>
+          );
+        }}
+      />
+    </StyledContainer>
+  );
 };
+
+const StyledContainer = styled("div")({
+  flexGrow: 1,
+});
 
 export default SearchableSelect;
